b5cf91097ccd3e8eeee3175ab2971aab26f3e126
lintangwisesa <lintangwisesa@ymail.com>
2019-01-05 07:14:08 +0700

    flask mongo

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 from flask_cors import CORS
+import yaml
 
 app = Flask(__name__)
+config = yaml.load(open('database.yaml'))
+client = MongoClient(config['uri'])
+db = client.lin_flask
+# db = client['lin_flask']
 CORS(app)
 
 @app.route('/')
